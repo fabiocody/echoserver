0bd24c5b747a255309d7e7b44d96dd50eabcca7b
Fabio Codiglioni <fabiocody@icloud.com>
2021-03-29 14:19:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,17 +6,14 @@ const app = express();
 app.use(morgan('[:method] :url (:status) - :res[content-length] B - :response-time ms'));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    const echo = {headers: req.headers, query: req.query};
-    res.send(echo);
-    res.on('finish', () => console.log(echo));
-});
+function echo(payload, res) {
+    res.send(payload);
+    res.on('finish', () => console.log(payload));
+}
 
-app.post('/', (req, res) => {
-    const echo = {headers: req.headers, body: req.body};
-    res.send(echo);
-    res.on('finish', () => console.log(echo));
-});
+app.route('/')
+    .get((req, res) => echo({headers: req.headers, query: req.query}, res))
+    .post((req, res) => echo({headers: req.headers, body: req.body}, res))
 
 const hostname = process.env.HOSTNAME || '0.0.0.0';
 const port = parseInt(process.env.PORT ||  '3000', 10);
