18587ff6ad311b6e178b8f41ef5f0bc0965202d6
Fabio Codiglioni <fabiocody@icloud.com>
2021-06-20 21:11:24 +0200

    Add ping and fibo endpoints

--- index.js
+++ index.js
@@ -13,9 +13,39 @@ function echo(payload, res) {
     });
 }
 
+function fibo(n) {
+    if (n === 0) {
+        return 0;
+    } else if (n === 1) {
+        return 1;
+    } else {
+        let grandparent = 0;
+        let parent = 1;
+        let me = -1;
+        for (let i = 2; i <= n; i++) {
+            me = grandparent + parent;
+            grandparent = parent;
+            parent = me;
+        }
+        return me;
+    }
+}
+
 app.route('/')
     .get((req, res) => echo({headers: req.headers, query: req.query}, res))
     .post((req, res) => echo({headers: req.headers, body: req.body}, res))
+
+app.get('/ping', (req, res) => res.send('PONG'));
+
+app.get('/fibo', (req, res) => {
+    if (req.query.n) {
+        const n = parseInt(req.query.n, 10);
+        res.send(fibo(n).toString());
+    } else {
+        res.sendStatus(400);
+    }
+});
+
 
 const hostname = process.env.HOSTNAME || '0.0.0.0';
 const port = parseInt(process.env.PORT ||  '3000', 10);
